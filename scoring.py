74105227dff691ccd68fd55e1ff879637fecb967
tfchan <cseelemon@gmail.com>
2019-04-17 21:03:42 +0800

    Finish batch folders extraction

---
+++ scoring.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Tool for scoring query homework and exam."""
+import os
+import argparse
+
+
+def directory(path):
+    """Check input path if it is valid directory."""
+    if path != '' and not os.path.isdir(path):
+        msg = f"{path} isn't a valid directory"
+        raise argparse.ArgumentTypeError(msg)
+    return path
+
+
+def all_dir():
+    """Return list of unhidden directorys in working directory."""
+    dirs = list(filter(os.path.isdir, os.listdir()))
+    dirs = [dir_ for dir_ in dirs if not dir_.startswith('.')]
+    return dirs
+
+
+def main():
+    """Perform main task."""
+    parser = argparse.ArgumentParser(
+        description='Query homework and exam scoring tool.')
+    parser.add_argument('-b', '--batches', type=directory,
+                        default=all_dir(), help='Batch to score')
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
