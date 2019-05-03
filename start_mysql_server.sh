1f1ab2cbbdd409a2915882d5f51e0027c94c826d
tfchan <cseelemon@gmail.com>
2019-05-04 01:17:11 +0800

    Fix start script to correctly wait for complete

--- start_mysql_server.sh
+++ start_mysql_server.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
+is_init_complete () {
+    docker logs some-mysql 2>&1 | grep "port: 3306  MySQL Community Server"
+}
+
 docker run --name some-mysql -e MYSQL_ROOT_PASSWORD="dbms" -d mysql:5.7
 if [ $? != 0 ]
 then
     exit 1
 fi
-complete_str="ready for connections"
 # Wait for init complete
-docker logs some-mysql 2>&1 | grep "ready for connections"
+is_init_complete
 while [ $? != 0 ]
 do
     sleep 1
-    docker logs some-mysql 2>&1 | grep "ready for connections"
+    is_init_complete
 done
 exit 0
