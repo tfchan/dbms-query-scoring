5fc176c8c60210a4410e71d7c3ee83ec0007b38e
tfchan <cseelemon@gmail.com>
2019-06-14 18:59:33 +0800

    Add class basic structure

--- mysql_docker.py
+++ mysql_docker.py
@@ -1,2 +1,11 @@
-"""Class for manipulating mysql docker container."""
+"""Class file for mysql docker."""
 import docker
+
+
+class MysqlDocker:
+    """Class for manipulating MySQL container."""
+
+    _client = docker.from_env()
+
+    def __init__(self):
+        """Imitialize everything."""
