e2b2ab4cc10c787d6ebb8bf8ff6436c6cb094a40
tfchan <cseelemon@gmail.com>
2019-07-02 13:23:54 +0800

    Add parameters to start server

--- mysql_docker.py
+++ mysql_docker.py
@@ -7,13 +7,18 @@ class MysqlDocker:
 
     _client = docker.from_env()
 
-    def __init__(self):
+    def __init__(self, start=True):
         """Imitialize everything."""
         self._server = None
+        self._root_pw = None
+        if start:
+            self.start_server()
 
-    def start_server(self):
+    def start_server(self, version='5.7', name=None, root_pw='admin'):
         """Start mysql server container."""
         if self._server is None:
             self._server = self._client.containers.run(
-                'mysql:5.7', detach=True, name='mysql-server',
-                environment=['MYSQL_ROOT_PASSWORD="dbms"'])
+                f'mysql:{version}', detach=True, name=name,
+                environment=[f'MYSQL_ROOT_PASSWORD="{root_pw}"'])
+            self._root_pw = root_pw
+            return self._server.short_id
