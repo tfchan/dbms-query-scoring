1b7fb5ece0ba1276931ca4e6d9abf02176bbe9cd
tfchan <cseelemon@gmail.com>
2019-09-20 13:10:37 +0800

    Add health check command to server

--- mysql_docker.py
+++ mysql_docker.py
@@ -17,8 +17,11 @@ class MysqlDocker:
     def start_server(self, version='5.7', name=None, root_pw='admin'):
         """Start mysql server container."""
         if self._server is None:
+            hc = docker.types.healthcheck.Healthcheck(
+                test=['CMD', 'mysqladmin', 'ping', '-h', 'localhost'])
             self._server = self._client.containers.run(
                 f'mysql:{version}', detach=True, name=name,
+                healthcheck=hc,
                 environment=[f'MYSQL_ROOT_PASSWORD="{root_pw}"'])
             self._root_pw = root_pw
             return self._server.short_id
